0fdd9f6259be6997703cdd11f53a5c3faa31b476
broncs <jhowfrombrazil@gmail.com>
2021-03-07 18:10:57 -0300

    adding structure to project and install dependencies

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+
+import circleImg from '../assets/x-circle.svg';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Fan wiki</title>
+      </Head>
+
+      <main>
+        <h1>hello world</h1>
+      </main>
+    </div>
+  );
+}
